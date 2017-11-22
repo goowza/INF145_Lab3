a8f10c23e47273cb71915db2675f50660ad1dc97
goowza <32903531+goowza@users.noreply.github.com>
2017-11-22 13:00:26 -0500

    Create t_file_etudiants.h

---
+++ t_file_etudiants.h
@@ -0,0 +1,33 @@
+#ifndef _T_FILE_ETUDIANTS__
+#define _T_FILE_ETUDIANTS__
+
+#include "structs.h"
+
+typedef t_etudiant t_element;
+
+typedef struct t_noeud
+{
+	t_element donnee;
+	struct t_noeud * suivant;
+}t_noeud;
+
+typedef struct
+{
+	t_noeud * tete;
+	int nb_elements;
+}t_file;
+
+void init_file(t_file * file);
+
+int file_vide(t_file * file);
+
+void ajout_debut(t_file * file, t_element element);
+
+void enfiler(t_file * file, t_element element);
+
+t_element desenfiler(t_file * file);
+
+void afficher_file(t_file * file);
+
+#endif
+
