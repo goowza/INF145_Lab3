73d3d74700e6740109d06317c021d3c1618cf675
goowza <32903531+goowza@users.noreply.github.com>
2017-11-25 14:08:10 -0500

    Update t_biblio_machine.h

--- t_biblio_machine.h
+++ t_biblio_machine.h
@@ -2,9 +2,24 @@
 #define _T_BIBLIO_MACHINE__
 
 #include "structs.h"
+#include "t_bibliotheque.h"
+#include "t_liste_livres.h"
 
-void servir_etudiant(t_etudiant * etudiant);
+#define NUM_CHOIX_RECHERCHE_MAX	5
+#define NUM_CHOIX_RECHERCHE_MIN	1
 
-void chercher_livre();
+
+void servir_etudiant(t_biblio_machine * machine,t_etudiant * etudiant);
+
+void chercher_livre(t_bibliotheque * pBibli);
+
+void afficher_menu_chercher_livre();
+
+int demander_choix_menu_machine();
+
+void afficher_liste_livres(t_liste * liste);
+
+//t_liste * rechercher_isbn(t_bibliotheque * pBibli, int isbn_demande);
 
 #endif
+
