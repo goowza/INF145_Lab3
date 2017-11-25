a582325a310f17d1b68acc1ee009072f3a730007
goowza <32903531+goowza@users.noreply.github.com>
2017-11-25 14:12:49 -0500

    Update test_machine.c

--- test_machine.c
+++ test_machine.c
@@ -1,11 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "t_biblio_machine.h"
 
+
 int main()
 {
 	t_etudiant e1 = { 1245,0,0 };
-	servir_etudiant(&e1);
+	t_biblio_machine * machine = NULL;
+	t_bibliotheque bibli;
+	initialiser_bibliotheque(&bibli);
+	gestion_bibliotheque(&bibli);
+	chercher_livre(&bibli);
+
 	system("PAUSE");
 	return EXIT_SUCCESS;
 }
