2fabcc5703ee2c83d176102bbb4d2329b379496c
goowza <32903531+goowza@users.noreply.github.com>
2017-11-28 14:35:32 -0500

    Update t_biblio_machine.h

--- t_biblio_machine.h
+++ t_biblio_machine.h
@@ -8,18 +8,26 @@
 #define NUM_CHOIX_RECHERCHE_MAX	5
 #define NUM_CHOIX_RECHERCHE_MIN	1
 
+#define NUM_CHOIX_SERVIR_MAX	4
+#define NUM_CHOIX_SERVIR_MIN	0
+
 
 void servir_etudiant(t_biblio_machine * machine,t_etudiant * etudiant);
 
-void chercher_livre(t_bibliotheque * pBibli);
+void chercher_livre(t_biblio_machine * machine);
+
+void retourner_livre(t_biblio_machine * machine);
+
+void afficher_menu_servir(t_etudiant * etudiant);
 
 void afficher_menu_chercher_livre();
 
-int demander_choix_menu_machine();
+void afficher_menu_retourner_livre(t_etudiant etudiant);
+
+int demander_choix(int borne_inf,int borne_sup);
 
 void afficher_liste_livres(t_liste * liste);
 
 //t_liste * rechercher_isbn(t_bibliotheque * pBibli, int isbn_demande);
 
 #endif
-
