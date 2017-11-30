3e7c1c1d44f4e4fc2fef687163c89fe1982c2617
goowza <32903531+goowza@users.noreply.github.com>
2017-11-30 17:31:19 -0500

    Update t_liste_livres.h

--- t_liste_livres.h
+++ t_liste_livres.h
@@ -1,34 +1,28 @@
+#pragma once
+
 #ifndef _T_LISTE_LIVRES__
 #define _T_LISTE_LIVRES__
 
 #include "structs.h"
-
-//changer cette déclaration pour obtenir des listes d'un autre type
-typedef t_livre t_element;
-
-typedef struct t_noeud
-{
-	t_element donnee;
-	struct t_noeud * suivant;
-}t_noeud;
-
-typedef struct
-{
-	t_noeud * tete;
-	int nb_elements;
-}t_liste;
+#include "t_bibliotheque.h"
 
 void init_liste(t_liste * liste);
 
 int liste_vide(t_liste * liste);
 
-void ajouter_debut(t_liste * liste, t_element element);
+int ajouter_debut(t_liste * liste, t_element_liste element);
 
-t_element enlever_debut(t_liste * liste);
+int ajouter_fin(t_liste * liste, t_element_liste element);
 
-void ajouter_liste_indice(t_liste * liste, t_element element, int indice);
+t_element_liste enlever_debut(t_liste * liste);
 
-t_element enlever_liste_indice(t_liste * liste, int indice);
+t_element_liste enlever_fin(t_liste * liste);
+
+int ajouter_liste_indice(t_liste * liste, t_element_liste element, int indice);
+
+t_element_liste enlever_liste_indice(t_liste * liste, int indice);
+
+//int ajouter_croissant(t_liste * liste, t_element_liste element);
 
 void afficher_liste(t_liste * liste);
 
