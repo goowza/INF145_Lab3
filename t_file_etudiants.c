3f635239cdf7c42ea88806381d0f9a71120c4ca3
goowza <32903531+goowza@users.noreply.github.com>
2017-11-30 17:38:21 -0500

    Update t_file_etudiants.c

--- t_file_etudiants.c
+++ t_file_etudiants.c
@@ -4,59 +4,68 @@
 
 void init_file(t_file * file)
 {
-	file->tete = NULL;
-	file->nb_elements = 0;
+	if (file != NULL)
+	{
+		file->tete = NULL;
+		file->nb_elements = 0;
+	}
 }
 
 int file_vide(const t_file * file)
 {
-	return(file->tete == NULL);
+	return(file->nb_elements == 0);
 }
 
-void ajout_debut(t_file * file, t_element element)
+void ajout_debut(t_file * file, t_element_file element)
 {
-	t_noeud * nouveau_noeud = (t_noeud*)malloc(sizeof(t_noeud));
-	nouveau_noeud->donnee = element;
-	nouveau_noeud->suivant = file->tete;
-	file->tete = nouveau_noeud;
-	file->nb_elements++;
-}
-
-void enfiler(t_file * file, t_element element)
-{
-	// pointeur permettant de parcourir la file
-	t_noeud * curseur = file->tete;
-
-	// pointeur vers le noeud a ajouter dans la file
-	t_noeud * nouveau_noeud = (t_noeud*)malloc(sizeof(t_noeud));
-	nouveau_noeud->donnee = element;
-
-	if(!file_vide(file))
+	t_noeud_file * nouveau_noeud = (t_noeud_file*)malloc(sizeof(t_noeud_file));
+	if (nouveau_noeud != NULL)
 	{
-		// aller a la fin de la liste
-		while (curseur->suivant != NULL)
-		{
-			curseur = curseur->suivant;
-		}
-		
-		// Mise a jour des liens
-		curseur->suivant = nouveau_noeud;
-		nouveau_noeud->suivant = NULL;
-
-		// mise a jour du nombre d'elements de la file
+		nouveau_noeud->donnee = element;
+		nouveau_noeud->suivant = file->tete;
+		file->tete = nouveau_noeud;
 		file->nb_elements++;
 	}
-	else
+}
+
+void enfiler(t_file * file, t_element_file element)
+{
+	// pointeur permettant de parcourir la file
+	t_noeud_file * curseur = file->tete;
+
+	// pointeur vers le noeud a ajouter dans la file
+	t_noeud_file * nouveau_noeud = (t_noeud_file*)malloc(sizeof(t_noeud_file));
+	nouveau_noeud->donnee = element;
+
+	if (nouveau_noeud != NULL)
 	{
-		ajout_debut(file, element);
+		if (!file_vide(file))
+		{
+			// aller a la fin de la liste
+			while (curseur->suivant != NULL)
+			{
+				curseur = curseur->suivant;
+			}
+
+			// Mise a jour des liens
+			curseur->suivant = nouveau_noeud;
+			nouveau_noeud->suivant = NULL;
+
+			// mise a jour du nombre d'elements de la file
+			file->nb_elements++;
+		}
+		else
+		{
+			ajout_debut(file, element);
+		}
 	}
 }
 
-t_element desenfiler(t_file * file)
+t_element_file desenfiler(t_file * file)
 {
-	t_element element_retire;
+	t_element_file element_retire;
 	// pointeur permettant de liberer la memoire
-	t_noeud * tmp = file->tete;
+	t_noeud_file * tmp = file->tete;
 
 	if (!file_vide(file))
 	{
@@ -65,12 +74,17 @@ t_element desenfiler(t_file * file)
 		free(tmp);
 		file->nb_elements--;
 	}
+	else
+	{
+		element_retire.annee_inscription = INVALIDE;
+		element_retire.ID = INVALIDE;
+	}
 	return element_retire;
 }
 
 void afficher_file(t_file * file)
 {
-	t_noeud * curseur = file->tete;
+	t_noeud_file * curseur = file->tete;
 	int compteur = 1;
 
 	if (!file_vide(file))
