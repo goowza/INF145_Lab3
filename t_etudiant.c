3fec82d6d7f0089006164c894d496a52f0d8ac8c
goowza <32903531+goowza@users.noreply.github.com>
2017-12-06 13:38:48 -0500

    Update t_etudiant.c

--- t_etudiant.c
+++ t_etudiant.c
@@ -12,18 +12,20 @@ void init_etudiant(t_etudiant * etudiant)
 	etudiant->annee_inscription = INVALIDE;
 	etudiant->ID = INVALIDE;
 
-	for(i=0;i<NB_LIVRES_EMPRUNTES_MAX;i++)
+	for (i = 0; i<NB_LIVRES_EMPRUNTES_MAX; i++)
 	{
 		etudiant->livres_empruntes[i] = livre_init;
 	}
 }
 
-void emprunter_livre_etudiant(t_etudiant * etudiant, t_livre livre)
+int emprunter_livre_etudiant(t_etudiant * etudiant, t_livre livre)
 {
 	int i = 0;
+	int resultat;
 	// verification du nombre d'emprunt
 	if (etudiant->livres_empruntes[NB_LIVRES_EMPRUNTES_MAX - 1].isbn != ISBN_INVALIDE)
 	{
+		resultat = ECHEC;
 		printf("Impossible d'emprunter un nouveau livre : limite atteinte\n");
 	}
 	else
@@ -34,8 +36,9 @@ void emprunter_livre_etudiant(t_etudiant * etudiant, t_livre livre)
 		}
 		// stockage du livre emprunte dans le tableau de l'etudiant
 		etudiant->livres_empruntes[i] = livre;
+		resultat = SUCCES;
 	}
-
+	return resultat;
 }
 
 void afficher_emprunts(t_etudiant etudiant)
