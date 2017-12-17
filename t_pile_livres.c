22b1201053681f57140758d4c46568059dc7467e
goowza <32903531+goowza@users.noreply.github.com>
2017-12-17 12:24:29 -0500

    Update t_pile_livres.c

--- t_pile_livres.c
+++ t_pile_livres.c
@@ -1,9 +1,36 @@
+// ========================= MODULE T_PILE_LIVRES ============================
+// Auteurs : Paul Meis (MEIP12039708) 
+//			(paul.meis.1@ens.etsmtl.ca)
+//			 Hervé Neugang (NEUR14027708) 
+//			(rodrigue-herve.neugang-tchientche.1@ens.etsmtl.ca)
+//
+//  Date : decembre 2017
+//
+//  Description : 
+//  Ce module permet de gerer une pile de livres via differentes fonctions.
+//
+//  Le module permet de :
+//	- Initialiser une pile de livres
+//  - Tester si une pile est vide.
+//	- Ajouter un livre au sommet de la pile.
+//	- Enlever un livre au sommet de la pile.
+//	- Afficher la pile.
+//  Attention : ce module gere uniquement des piles de t_livres
+//=============================================================================
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "t_pile_livres.h"
 
-
+//-----------------------------------------------------------------------------
+//							Fonction init_pile
+// Cette fonction permet d'initialiser une une pile, la pile est vide et son 
+// sommet pointe vers NULL.
+//
+// Parametres : pile = la pile a initialiser
+// Retour :		aucun
+//-----------------------------------------------------------------------------
 void init_pile(t_pile * pile)
 {
 	if (pile != NULL)
@@ -13,11 +40,27 @@ void init_pile(t_pile * pile)
 	}
 }
 
+//-----------------------------------------------------------------------------
+//							Fonction pile_vide
+// Cette fonction renvoie si une pile est vide ou non. 
+// (La fonction renvoie 0 si la pile n'est pas vide)
+//
+// Parametres : pile = la pile a tester
+// Retour :		resultat du test
+//-----------------------------------------------------------------------------
 int pile_vide(t_pile * pile)
 {
 	return (pile->nb_elements == 0);
 }
 
+//-----------------------------------------------------------------------------
+//							Fonction push
+// Cette fonction ajoute un element au sommet de la pile.
+//
+// Parametres : pile = la pile ou ajouter l'element
+//				element = element a ajouter a la pile
+// Retour :		aucun
+//-----------------------------------------------------------------------------
 void push(t_pile * pile, t_element_pile element)
 {
 	// pointeur vers l'element a ajouter
@@ -32,15 +75,22 @@ void push(t_pile * pile, t_element_pile element)
 		nouveau_noeud->suivant = pile->sommet;
 		pile->sommet = nouveau_noeud;
 
-		// mise a jour nombre d'elements total de la liste
+		// mise a jour nombre d'elements total de la pile
 		pile->nb_elements++;
 	}
-
 }
 
+//-----------------------------------------------------------------------------
+//							Fonction pop
+// Cette fonction enleve un element au sommet de la pile.
+// La fonction retourne une copie de l'element enleve.
+//
+// Parametres : pile = la pile ou enlever l'element
+// Retour :		element_retire = copie de l'element enleve de la pile
+//-----------------------------------------------------------------------------
 t_element_pile pop(t_pile * pile)
 {
-	// element enleve de la liste
+	// element enleve de la pile
 	t_element_pile element_retire;
 	// pointeur permettant de free la memoire
 	t_noeud_pile * tmp;
@@ -58,12 +108,19 @@ t_element_pile pop(t_pile * pile)
 		pile->sommet = pile->sommet->suivant;
 		free(tmp);
 
-		// mise a jour nombre d'elements total de la liste
+		// mise a jour nombre d'elements total de la pile
 		pile->nb_elements--;
 	}
 	return element_retire;
 }
 
+//-----------------------------------------------------------------------------
+//							Fonction afficher_pile
+// Cette fonction affiche tous les elements d'une pile depuis son sommet.
+//
+// Parametres : pile = la pile a afficher
+// Retour :		aucun
+//-----------------------------------------------------------------------------
 void afficher_pile(t_pile * pile)
 {
 	t_pile pile_temp;
@@ -73,12 +130,14 @@ void afficher_pile(t_pile * pile)
 
 	if (pile != NULL)
 	{
+		// desempiler la pile pour acceder a ses valeurs
 		while (!pile_vide(pile))
 		{
 			livre_temp = pop(pile);
 			afficher_info_livre(&livre_temp);
 			push(&pile_temp, livre_temp);
 		}
+		// re - empiler la pile de sorte a revenir a sa valeur initiale
 		while (!pile_vide(&pile_temp))
 		{
 			livre_temp = pop(&pile_temp);
